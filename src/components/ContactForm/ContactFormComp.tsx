47e2dacd3de4451d81f44ddeebbffe78a6bb42c5
Matt Harrell <matthew.629@hotmail.com>
2022-07-02 13:56:50 -0400

    add material UI form components

--- src/components/ContactForm/ContactFormComp.tsx
+++ src/components/ContactForm/ContactFormComp.tsx
@@ -1,3 +1,4 @@
+import { Grid,FormControl,TextField, Button, Container } from '@mui/material';
 
 interface contactFormCompProps {
     name:string;
@@ -12,15 +13,53 @@ interface contactFormCompProps {
 const ContFormComp = (props:contactFormCompProps) => {
 
     return(
-    <form onSubmit={props.sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" value={props.name} onChange={props.handleNameChange} />
-      <label>Email</label>
-      <input type="email" name="user_email" value={props.email} onChange={props.handleEmailChange} />
-      <label>Message</label>
-      <textarea name="message"  value={props.message} onChange={props.handleMessageChange}/>
-      <input type="submit" value="Send" />
-    </form>
+    <Container>
+        <FormControl fullWidth>
+            <TextField
+                type="text" 
+                name="user_name"
+                required 
+                value={props.name} 
+                onChange={props.handleNameChange}
+                label="Name"
+                placeholder='Jane Doe'
+            />
+            {/* <input type="text" name="user_name" value={props.name} onChange={props.handleNameChange} /> */}
+            <TextField
+                type="text" 
+                name="user_email"
+                required 
+                value={props.email} 
+                onChange={props.handleEmailChange}
+                label="Email"
+                placeholder='example@example.com'
+            />
+            {/* <input type="email" name="user_email" value={props.email} onChange={props.handleEmailChange} /> */}
+            {/* <label>Message</label> */}
+            <TextField
+                type="text" 
+                name="message"
+                required
+                multiline
+                rows={4} 
+                value={props.message} 
+                onChange={props.handleMessageChange}
+                label="Message"
+                placeholder='Please leave a detailed message'
+                variant='filled'
+            />
+            <Button variant="contained" 
+                disableElevation
+                type='submit'
+                color='secondary'
+                onClick={props.sendEmail}
+            >
+                Submit
+            </Button>
+            {/* <textarea name="message"  value={props.message} onChange={props.handleMessageChange}/> */}
+            {/* <input type="submit" value="Send" /> */}
+        </FormControl>
+    </Container>
     );
 
 }
